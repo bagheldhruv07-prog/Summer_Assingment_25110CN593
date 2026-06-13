90e6c28e152613a6589090088a910802d70c7712
Dhruv Baghel <bagheldhruv07@gmail.com>
2026-06-14 01:21:20 +0530

    Day-8 completed

--- Day-8/Q31.c
+++ Day-8/Q31.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int rows,i,j;
-    printf("Enter the number of rows: ");
-    scanf("%d", &rows);
+    int rows=5,i,j;
     for(i = 1; i <= rows; i++)
     {
         for(j = 1; j <= i; j++)
