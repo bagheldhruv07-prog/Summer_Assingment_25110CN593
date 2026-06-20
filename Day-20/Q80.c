e8ea78d1053945233155139b780b091cda8952a6
Dhruv Baghel <bagheldhruv07@gmail.com>
2026-06-20 22:19:12 +0530

    Day-14 completed

---
+++ Day-20/Q80.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10], rows, cols, i, j, sum;
+
+    printf("Enter rows and columns of matrix: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter the elements of matrix:\n");
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < cols; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Row-wise sum:\n");
+    for (i = 0; i < rows; i++)
+    {
+        sum = 0;
+        for (j = 0; j < cols; j++)
+        {
+            sum = sum + a[i][j];
+        }
+        printf("Sum of row %d = %d\n", i + 1, sum);
+    }
+
+    return 0;
+}
